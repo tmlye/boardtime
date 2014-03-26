a4b59c963f2f80886fe90fe51eb7d1e753828fe2
Sascha Eglau <sascha.eglau@gmail.com>
2014-03-26 23:12:40 +0100

    refactor

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,68 +1,26 @@
 var canvas = document.getElementById('board');
 var context = canvas.getContext('2d');
 
-var lastMouse = {
-    x: 0,
-    y: 0
-};
-
 // attach the mousedown, mouseout, mousemove, mouseup event listeners.
 canvas.addEventListener('mousedown', function (e) {
-  lastMouse = {
-    x: e.pageX - this.offsetLeft,
-    y: e.pageY - this.offsetTop
-  };
-  canvas.addEventListener('mousemove', move, false);
+  activeTool.mouseDownHandler(e);
+  // emitEvent('mouseDown', {});
+  canvas.addEventListener('mousemove', function(e) {
+      activeTool.mouseDragHandler(e);
+      onMouseMove(e);
+  } , false);
 }, false);
 
 canvas.addEventListener('mouseout', function () {
-  canvas.removeEventListener('mousemove', move, false);
+  canvas.removeEventListener('mousemove', onMouseMove, false);
 }, false);
 
 canvas.addEventListener('mouseup', function () {
-  canvas.removeEventListener('mousemove', move, false);
+  canvas.removeEventListener('mousemove', onMouseMove, false);
 }, false);
 
-// Draws the lines, called by move and the TogetherJS event listener:
-function draw(start, end, color, size, compositeOperation, save) {
-  context.save();
-  context.lineJoin = 'round';
-  context.lineCap = 'round';
-  // Since the coordinates have been translated to an 1140x400 canvas, the context needs to be scaled before it can be drawn on:
-  // context.scale(canvas.width/1140,canvas.height/400);
-  context.strokeStyle = color;
-  context.globalCompositeOperation = compositeOperation;
-  context.lineWidth = size;
-  context.beginPath();
-  context.moveTo(start.x, start.y);
-  context.lineTo(end.x, end.y);
-  context.closePath();
-  context.stroke();
-  context.restore();
-  if (save) {
-    // Won't save if draw() is called from reDraw().
-    // lines.push([{x: start.x, y: start.y}, {x: end.x, y: end.y}, color, size, compositeOperation]);
-  }
-}
-
-// Called whenever the mousemove event is fired, calls the draw function:
-function move(e) {
-  var mouse = {
-    x: e.pageX - this.offsetLeft,
-    y: e.pageY - this.offsetTop
-  };
-  // Translates the coordinates from the local canvas size to 1140x400:
-  sendMouse = {
-    x: (1140/canvas.width)*mouse.x,
-    y: (400/canvas.height)*mouse.y
-  };
-  sendLastMouse = {
-    x: (1140/canvas.width)*lastMouse.x,
-    y: (400/canvas.height)*lastMouse.y
-  };
-  draw(lastMouse, mouse, context.strokeStyle, context.lineWidth, context.globalCompositeOperation, true);
-  emitEvent('mouseDrag', {toolId: activeTool.toolId, point: { x: mouse.x, y: mouse.y }, lastPoint: { x: lastMouse.x, y: lastMouse.y }, color: context.strokeStyle, line: context.lineWidth, globalCompositeOperation: context.globalCompositeOperation});
-  lastMouse = mouse;
+function onMouseMove(e) {
+      emitEvent('mouseDrag', {toolId: activeTool.toolId, point: { x: mouse.x, y: mouse.y }, lastPoint: { x: lastMouse.x, y: lastMouse.y }, color: context.strokeStyle, line: context.lineWidth, globalCompositeOperation: context.globalCompositeOperation});
 }
 
 // global reference to all tools
--- public/javascripts/pencil.js
+++ public/javascripts/pencil.js
@@ -3,6 +3,11 @@ var pencil = function(context) {
     var that = {};
     that.context = context;
 
+    that.lastMouse = {
+        x: 0,
+        y: 0
+    };
+
     that.activate = function() {
         // brush settings
         context.lineWidth = 2;
@@ -13,36 +18,54 @@ var pencil = function(context) {
 
     that.toolId = 0;
 
-    that.mouseDragHandler = function(data) {
-        path.add(data.point);
+    that.mouseDragHandler = function(e) {
+      var mouse = {
+        x: e.pageX - this.offsetLeft,
+        y: e.pageY - this.offsetTop
+      };
+      // Translates the coordinates from the local canvas size to 1140x400:
+      sendMouse = {
+        x: (1140/canvas.width)*mouse.x,
+        y: (400/canvas.height)*mouse.y
+      };
+      sendLastMouse = {
+        x: (1140/canvas.width)*lastMouse.x,
+        y: (400/canvas.height)*lastMouse.y
+      };
+      draw(lastMouse, mouse, context.strokeStyle, context.lineWidth, context.globalCompositeOperation, true);
+      lastMouse = mouse;
     };
 
-    that.mouseDownHandler = function(data) {
-        path = new Path();
-        path.strokeColor = 'black';
-        path.add(data.point);
-        view.draw();
+    that.mouseDownHandler = function(e) {
+      lastMouse = {
+        x: e.pageX - this.offsetLeft,
+        y: e.pageY - this.offsetTop
+      };
     };
 
-    that.mouseUpHandler = function(data) {
+    that.mouseUpHandler = function(e) {
     };
 
-    // Move this to superclass
-    // EVENTS
-    that.onMouseDown = function onMouseDown(event) {
-        that.mouseDownHandler(event);
-        emitEvent('mouseDown', { toolId: 0, point: { x: event.point.x, y: event.point.y }});
-    };
-
-    that.onMouseDrag = function(event) {
-        that.mouseDragHandler(event);
-        emitEvent('mouseDrag', { toolId: 0, point: { x: event.point.x, y: event.point.y }});
-    };
-
-    that.onMouseUp = function(event) {
-        that.mouseUpHandler(event);
-        emitEvent('mouseDown', { toolId: 0, point: { x: event.point.x, y: event.point.y }});
-    };
+    that.draw = function draw(start, end, color, size, compositeOperation, save) {
+      context.save();
+      context.lineJoin = 'round';
+      context.lineCap = 'round';
+      // Since the coordinates have been translated to an 1140x400 canvas, the context needs to be scaled before it can be drawn on:
+      // context.scale(canvas.width/1140,canvas.height/400);
+      context.strokeStyle = color;
+      context.globalCompositeOperation = compositeOperation;
+      context.lineWidth = size;
+      context.beginPath();
+      context.moveTo(start.x, start.y);
+      context.lineTo(end.x, end.y);
+      context.closePath();
+      context.stroke();
+      context.restore();
+      if (save) {
+        // Won't save if draw() is called from reDraw().
+        // lines.push([{x: start.x, y: start.y}, {x: end.x, y: end.y}, color, size, compositeOperation]);
+      }
+    }
 
     return that;
 };
